61bcb1c32048a78a6cacb40c0f553f6ada30c263
Sammuel <78372969+DevSammuel@users.noreply.github.com>
2021-02-01 15:49:05 -0300

    Add files via upload

---
+++ src/testefuncao02/TesteFuncao02.java
@@ -0,0 +1,13 @@
+
+package testefuncao02;
+
+public class TesteFuncao02 {
+
+    public static void main(String[] args) {
+        System.out.println("Vai começar a contagem");
+        System.out.println(operacoes.contador(1,5));
+     
+        
+    }
+    
+}
