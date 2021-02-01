7571e85ed9df76054f35916bea3d7a3c64f1f0c6
Sammuel <78372969+DevSammuel@users.noreply.github.com>
2021-02-01 15:48:25 -0300

    Add files via upload

---
+++ src/testefuncao01/TesteFuncao01.java
@@ -0,0 +1,19 @@
+
+package testefuncao01;
+
+public class TesteFuncao01 {
+    
+    static int soma(int a, int b){
+        int s = a + b;
+        return s;
+    }
+
+    public static void main(String[] args) {
+        int sm = soma(5,2); // invoca a soma
+        System.out.println("a soma vale "+ sm);
+        
+ 
+        
+    }
+    
+}
